8e4254d8baf051b1d8d201d08b784d41f116bb73
pawan <pawangrover904@gmail.com>
2026-07-26 23:28:18 +0530

    today 20 questions

--- main.js
+++ main.js
@@ -384,22 +384,23 @@
 // console.log(newarr);
 
 // Q20. 🔥 Ultimate Interview Challenge
-const employees = [
-  { id: 1, name: "Pawan", salary: 25000, active: true },
-  { id: 2, name: "Rahul", salary: 45000, active: false },
-  { id: 3, name: "Amit", salary: 55000, active: true },
-  { id: 4, name: "Neha", salary: 35000, active: true },
-  { id: 5, name: "Karan", salary: 60000, active: false },
-];
+// const employees = [
+//   { id: 1, name: "Pawan", salary: 25000, active: true },
+//   { id: 2, name: "Rahul", salary: 45000, active: false },
+//   { id: 3, name: "Amit", salary: 55000, active: true },
+//   { id: 4, name: "Neha", salary: 35000, active: true },
+//   { id: 5, name: "Karan", salary: 60000, active: false },
+// ];
 
-const arr1=employees.filter((n)=>{
-    return n.active===true
-})
-console.log(arr1)
+// const arr1 = employees.filter((n) => {
+//   return n.active === true;
+// });
+// console.log(arr1);
 
-const arr2=employees.find(item => item)((n)=>{
-    return n.name})
-console.log(arr2)
+// const arr2 = employees.find((item) => item)((n) => {
+//   return n.name;
+// });
+// console.log(arr2);
 // Is array par ye sab solve karo:
 
 // Active employees nikalo.
@@ -414,3 +415,192 @@ console.log(arr2)
 // Salary ascending sort karo.
 // Salary descending sort karo.
 // Sabhi names uppercase me convert karo.
+
+// Q1. map()
+// Har number ka cube nikalo.
+// const numbers = [1, 2, 3, 4, 5];
+// const arr = numbers.map((item) => item ** item);
+
+// console.log(arr);
+// Q2. map()
+// Har object me ek nayi property adult: true add karo.
+// const users = [
+//   { name: "Pawan", age: 22 },
+//   { name: "Rahul", age: 20 },
+//   { name: "Amit", age: 25 },
+// ];
+// const arr=users.map((item )=>{
+//     return{...item,adult:true}
+// })
+// console.log(arr)
+
+// Q3. filter()
+// Sirf pass marks (>= 50) nikalo.
+// const marks = [95, 45, 67, 32, 88, 76];
+// const arr = marks.filter((item) => item >= 50);
+// console.log(arr);
+
+// Q4. filter()
+// Sirf IT department wale employees nikalo.
+// const employees = [
+//   { name: "A", department: "IT" },
+//   { name: "B", department: "HR" },
+//   { name: "C", department: "IT" },
+// ];
+// const arr = employees.filter((item) => item.department === "IT");
+// console.log(arr);
+
+// Q5. find()
+// Title "React" wali book find karo.
+// const books = [
+//   { id: 1, title: "JavaScript" },
+//   { id: 2, title: "React" },
+//   { id: 3, title: "Node.js" },
+// ];
+
+// const arr = books.find((item) => item.title === "React");
+// console.log(arr);
+
+// Q6. findIndex()
+// "Green" ka index nikalo.
+// const colors = ["Red", "Blue", "Green", "Yellow"];
+// const arr = colors.findIndex((item) => item === "Green");
+// console.log(arr);
+
+// Q7. includes()
+// Check karo "Guest" exist karta hai ya nahi.
+// const roles = ["Admin", "User", "Editor"];
+// const arr = roles.includes("Gruest");
+// console.log(arr);
+
+// Q8. some()
+// Check karo kya kisi ki salary 60000 se jyada hai.
+// const salaries = [20000, 30000, 45000, 70000];
+// const arr = salaries.some((n) => n > 60000);
+// console.log(arr);
+// Q9. every()
+// Check karo kya sabhi users 18 ya usse bade hain.
+// const ages = [21, 25, 18, 30];
+// const arr = ages.every((n) => n >= 18);
+// console.log(arr);
+
+// Q10. reduce()
+// // Sabhi numbers ka product (multiplication) nikalo.
+// const numbers = [2, 4, 6, 8];
+// const arr = numbers.reduce((a, b) => a * b);
+// console.log(arr);
+// Q11. reduce()
+// Total bill calculate karo.
+// const cart = [
+//   { name: "Pen", price: 20 },
+//   { name: "Book", price: 100 },
+//   { name: "Bag", price: 500 },
+// ];
+// const arr = cart.reduce((a, b) => a + b.price,0);
+// console.log(arr);
+// Q12. sort()
+// Ascending order me sort karo.
+// const numbers = [15, 2, 100, 45, 7];
+// const arr=numbers.sort((a,b)=>a-b)
+// console.log(arr)
+
+// Q13. sort()
+// Alphabetically sort karo.
+// const cities = ["Delhi", "Agra", "Mumbai", "Chennai"];
+// const arr = cities.sort();
+// console.log(arr);
+
+// Q14. reverse()
+// Reverse order me print karo.
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const arr = months.reverse();
+// console.log(arr);
+
+// Q15. slice()
+// Sirf [15, 20, 25] ka naya array banao.
+// const data = [5, 10, 15, 20, 25, 30];
+// const arr=data.slice(2,5)
+// console.log(arr)
+// Q16. splice()
+// "Orange" remove karke uski jagah "Grapes" insert karo.
+// const fruits = ["Apple", "Banana", "Mango", "Orange"];
+// const arr = fruits.splice(3, 1);
+// console.log(arr);
+// console.log(fruits);
+// Q17. concat()
+// Teeno arrays ko merge karo.
+// const html = ["HTML"];
+// const css = ["CSS"];
+// const js = ["JavaScript"];
+// const arr = html.concat(css, js);
+// console.log(arr);
+
+// Q18. forEach()
+// Print karo:
+// const students = [
+//   { name: "Ram", marks: 90 },
+//   { name: "Shyam", marks: 80 },
+//   { name: "Mohan", marks: 70 },
+// ];
+// const result = students.map((student) => {
+//   return `${student.name} Score ${student.marks}`;
+// });
+
+// console.log(result);
+
+// Ram scored 90
+// Shyam scored 80
+// Mohan scored 70
+// Q19. flat()
+// Array ko completely flat karo.
+// const arr = [1, [2, [3, [4, [5]]]]];
+// const newarr=arr.flat(Infinity)
+// console.log(newarr)
+
+// Q20. ⭐ Ultimate Interview Challenge
+const employees = [
+  { id: 1, name: "Pawan", salary: 25000, active: true, department: "IT" },
+  { id: 2, name: "Rahul", salary: 45000, active: false, department: "HR" },
+  { id: 3, name: "Amit", salary: 55000, active: true, department: "IT" },
+  { id: 4, name: "Neha", salary: 35000, active: true, department: "Sales" },
+  { id: 5, name: "Karan", salary: 60000, active: false, department: "IT" },
+];
+
+// Is array par ye sab solve karo:
+
+// Sirf active employees nikalo.
+// const arr = employees.filter((item) => item.active === true);
+// console.log(arr);
+// Sirf IT department wale employees nikalo.
+// const arr = employees.filter((item) => item.department === "IT");
+// console.log(arr);
+// Sirf names ka array banao.
+// const arr = employees.map(item => item.name);
+// console.log(arr)
+// Total salary calculate karo.
+// const arr = employees.reduce((a, b) => a + b.salary, 0);
+// console.log(arr);
+// Average salary nikalo.
+// const arr = employees.reduce((a, b) => a + b.salary, 0)/employees.length;
+// console.log(arr)
+
+// // Highest salary nikalo (reduce()).
+// const highestSalary = employees.reduce((max, employee) => {
+//   return employee.salary > max ? employee.salary : max;
+// }, 0);
+
+// console.log(highestSalary);
+// // Lowest salary nikalo (reduce()).
+// const lowestSalary = employees.reduce((min, employee) => {
+//   return employee.salary < min ? employee.salary : min;
+// }, employees[0].salary);
+
+// console.log(lowestSalary);
+// id = 4 employee find karo.
+// "Rahul" ka index find karo.
+// Check karo kya koi salary 80000 se jyada hai.
+// Check karo kya sabhi employees active hain.
+// Salary ascending sort karo.
+// Salary descending sort karo.
+// Names uppercase me convert karo.
+// Method chaining: Active employees → IT department → Names uppercase → Alphabetically sort.
